e16ceb02441cb18cb30580f9eaa2d1967c8fc7a0
YoungJoo Lee <youngker@gmail.com>
2022-03-12 00:01:39 +0900

    add keyboard shortcut for hangul(shift space)

--- nix/config/darwin-settings.nix
+++ nix/config/darwin-settings.nix
@@ -13,6 +13,9 @@ with lib; {
 
       defaults write 'Apple Global Domain' _HIHideMenuBar -float 1
       defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag 0
+
+      /usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:60:enabled true" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+      /usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:60:value:parameters:2 131072" ~/Library/Preferences/com.apple.symbolichotkeys.plist
     '';
   };
 }
