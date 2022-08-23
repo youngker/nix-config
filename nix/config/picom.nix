4c93cac2fc9c6d30961de8f0692b683aa0403579
YoungJoo Lee <youngker@gmail.com>
2022-08-23 20:51:52 +0900

    fix build error

--- nix/config/picom.nix
+++ nix/config/picom.nix
@@ -7,26 +7,10 @@ with lib; {
       #   #!/bin/sh
       #   ${pkgs.nixGL}/bin/nixGL ${pkgs.picom}/bin/picom "$@"
       # '';
-      experimentalBackends = true;
-      blur = true;
-      blurExclude = [ "window_type = 'dock'" "window_type = 'desktop'" ];
-
       fade = true;
       fadeDelta = 5;
-
       shadow = true;
-      shadowOffsets = [ (-7) (-7) ];
-      shadowOpacity = "0.8";
-      shadowExclude = [ "window_type *= 'normal' && ! name ~= ''" ];
-      noDockShadow = false;
-      noDNDShadow = true;
-
-      activeOpacity = "1.0";
-      inactiveOpacity = "0.8";
-      menuOpacity = "0.8";
-
       backend = "glx";
-      vSync = true;
     };
   };
 }
