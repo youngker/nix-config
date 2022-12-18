98ae19647b0f336d08b88b7eb841f0d506b9c514
YoungJoo Lee <youngker@gmail.com>
2022-12-18 19:56:29 +0900

    fix build error

--- nix/packages/bingwallpaper.nix
+++ nix/packages/bingwallpaper.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, rustPlatform, fetchFromGitHub, openssl, darwin, ... }:
+{ lib, pkgs, rustPlatform, fetchFromGitHub, pkg-config, openssl, darwin, ... }:
 
 rustPlatform.buildRustPackage rec {
   pname = "bingwallpaper";
@@ -10,6 +10,8 @@ rustPlatform.buildRustPackage rec {
     rev = "9db629ed15e1e441637ea1e72099264aba44d6bf";
     sha256 = "sha256-QKvLw1I2WVa1gsk8yVEL+5dKuFa2Kk6awjTc6G5lE8c=";
   };
+
+  nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [ openssl ] ++ (lib.optionals pkgs.stdenv.isDarwin
     [ darwin.apple_sdk.frameworks.Security ]);
