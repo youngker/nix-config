34e2641a13b4aaae92b5a1fb130be5c92730a53b
YoungJoo Lee <youngker@gmail.com>
2023-01-03 22:35:20 +0900

    cleanup

--- darwin/modules/services/skhd.nix
+++ darwin/modules/services/skhd.nix
@@ -11,6 +11,9 @@ with lib; {
   config = mkIf config.modules.services.skhd.enable {
     services.skhd = {
       enable = true;
+      skhdConfig = ''
+        shift + alt - return : open -na ${pkgs.alacritty}/Applications/Alacritty.app
+      '';
     };
   };
 }
