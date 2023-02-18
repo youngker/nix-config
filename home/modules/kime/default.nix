4926765babe3091b8ef4a47071bec12c828acb66
YoungJoo Lee <youngker@gmail.com>
2023-02-18 11:53:27 +0900

    update kime configuration

--- home/modules/kime/default.nix
+++ home/modules/kime/default.nix
@@ -12,16 +12,6 @@ in {
 
   config = mkIf cfg.enable {
     i18n.inputMethod.enabled = "kime";
-    i18n.inputMethod.kime.config = {
-      engine = {
-        global_hotkeys = {
-          S-Space = {
-            behavior = { Toggle = [ "Hangul" "Latin" ]; };
-            result = "Consume";
-          };
-        };
-        hangul = { layout = "dubeolsik"; };
-      };
-    };
+    xdg.configFile."kime".source = ./kime;
   };
 }
