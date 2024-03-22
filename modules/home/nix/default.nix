2a41ade6ae98b257058ee2d94d65d6fba035ccb7
YoungJoo Lee <youngker@gmail.com>
2024-03-23 00:38:21 +0900

    update flake

--- modules/home/nix/default.nix
+++ modules/home/nix/default.nix
@@ -11,6 +11,9 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ rnix-lsp nixpkgs-fmt ];
+    home.packages = with pkgs; [
+      # rnix-lsp
+      nixpkgs-fmt
+    ];
   };
 }
