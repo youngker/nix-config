bf6a30e15446dbda3a565f196c78b7e2cdf404c5
YoungJoo Lee <youngker@gmail.com>
2024-12-16 21:12:27 +0900

    update firefox version

--- modules/darwin/yabai/default.nix
+++ modules/darwin/yabai/default.nix
@@ -23,24 +23,28 @@ in
       config = {
         active_window_opacity = 1.0;
         auto_balance = "off";
+        bottom_padding = 2;
         debug_output = "on";
         focus_follows_mouse = "off";
         layout = "bsp";
+        left_padding = 2;
+        mouse_action1 = "move";
+        mouse_action2 = "resize";
+        mouse_drop_action = "swap";
         mouse_follows_focus = "off";
+        mouse_modifier = "alt";
         normal_window_opacity = 0.85;
-        split_ratio = 0.62;
+        right_padding = 2;
+        split_ratio = 0.50;
+        split_type = "auto";
+        top_padding = 2;
         window_border = "off";
+        window_gap = 2;
         window_opacity = "on";
         window_opacity_duration = 0.0;
         window_placement = "second_child";
         window_shadow = "on";
         window_topmost = "off";
-
-        top_padding = 2;
-        bottom_padding = 2;
-        left_padding = 2;
-        right_padding = 2;
-        window_gap = 2;
       };
 
       extraConfig = ''
--- packages/firefox.nix
+++ packages/firefox.nix
@@ -8,11 +8,11 @@
 with pkgs;
 stdenv.mkDerivation rec {
   pname = "Firefox";
-  version = "130.0.1";
+  version = "133.0.3";
   src = fetchurl {
     name = "Firefox-${version}.dmg";
     url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/Firefox%20${version}.dmg";
-    sha256 = "sha256-Y+2HhIXVSYwmnZW6fmTxEE7QhbjjMLDvClZfhcxgNCY=";
+    sha256 = "sha256-nOtPohICKPKH5sZUzveJi0zOCmWScAVidriIRYEmfTs=";
   };
   buildInputs = [
     undmg
